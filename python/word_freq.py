0de3d360c3558a0dc71e278dbfedd209fa7c8ac9
Oh seungmin <tmdals179@gmail.com>
2022-04-08 17:53:27 +0900

    separate into words

--- python/word_freq.py
+++ python/word_freq.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import sys
-
+import re
 if __name__=='__main__':
 	f = open(sys.argv[1],"r")
-	print(f.read())
+	f = f.readlines()
+	#print(f)
+	for lines in f:
+		words = re.split('\W+',lines)
+		print(words)

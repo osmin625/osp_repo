e8dbc671effec9e65e83524bf40c3efaa80c4e05
Oh seungmin <tmdals179@gmail.com>
2022-04-08 18:28:28 +0900

    dictionary sorted

--- python/word_freq.py
+++ python/word_freq.py
@@ -13,6 +13,7 @@ if __name__=='__main__':
 	#print(wordlist)
 	for w in keywords:
 		freq[w] = wordlist.count(w)
+	freq = dict(sorted(freq.items(),key=lambda item: item[1]))
 	for key, val in freq.items():
 	    print("{key} {value}".format(key=key,value=val))
 
